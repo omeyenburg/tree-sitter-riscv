0e9d65ce802547a42ac3b80cbaa93d498d056713
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2024-12-28 12:15:08 +0100

    feat(rust binding): add language() shortcut

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -7,6 +7,18 @@
 //! let code = r#"
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
+//! parser
+//!     .set_language(tree_sitter_mips::language())
+//!     .expect("Error loading Mips parser");
+//! let tree = parser.parse(code, None).unwrap();
+//! ```
+//!
+//! or
+//!
+//! ```
+//! let code = r#"
+//! "#;
+//! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_mips::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
@@ -19,6 +31,7 @@
 //! [tree-sitter]: https://tree-sitter.github.io/
 
 use tree_sitter_language::LanguageFn;
+use tree_sitter::Language;
 
 extern "C" {
     fn tree_sitter_mips() -> *const ();
@@ -29,15 +42,18 @@ extern "C" {
 /// [LanguageFn]: https://docs.rs/tree-sitter-language/*/tree_sitter_language/struct.LanguageFn.html
 pub const LANGUAGE: LanguageFn = unsafe { LanguageFn::from_raw(tree_sitter_mips) };
 
+pub fn language() -> Language {
+    return LANGUAGE.into();
+}
+
 /// The content of the [`node-types.json`][] file for this grammar.
 ///
 /// [`node-types.json`]: https://tree-sitter.github.io/tree-sitter/using-parsers#static-node-types
 pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
-// NOTE: uncomment these to include any queries that this grammar contains:
-
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
-// pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
+// The included queries in this grammar.
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
 // pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 // pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
 
