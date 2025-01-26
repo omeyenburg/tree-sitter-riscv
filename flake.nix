327f41f013ce07fdd55b734ff5edbf4d789deb4f
omeyemburg <135001586+omeyenburg@users.noreply.github.com>
2025-01-26 08:32:45 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Tree-sitter development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            tree-sitter
+            nodejs
+          ];
+        };
+      }
+    );
+}
+
+# vim: sw=2 sts=2 et
