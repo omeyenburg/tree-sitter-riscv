1726e85a342b71f61695cea6143b3361609f31a0
omeyemburg <oskar.git@gmx.net>
2025-08-13 00:40:59 +0200

    build: revert to skip macos

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #endif
 
-#define LANGUAGE_VERSION 14
+#define LANGUAGE_VERSION 15
 #define STATE_COUNT 398
 #define LARGE_STATE_COUNT 2
 #define SYMBOL_COUNT 104
@@ -2514,7 +2514,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   }
 }
 
-static const TSLexMode ts_lex_modes[STATE_COUNT] = {
+static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0, .external_lex_state = 1},
   [1] = {.lex_state = 35},
   [2] = {.lex_state = 35},
@@ -12953,6 +12953,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_mips(void) {
     .state_count = STATE_COUNT,
     .large_state_count = LARGE_STATE_COUNT,
     .production_id_count = PRODUCTION_ID_COUNT,
+    .supertype_count = SUPERTYPE_COUNT,
     .field_count = FIELD_COUNT,
     .max_alias_sequence_length = MAX_ALIAS_SEQUENCE_LENGTH,
     .parse_table = &ts_parse_table[0][0],
@@ -12979,6 +12980,13 @@ TS_PUBLIC const TSLanguage *tree_sitter_mips(void) {
       tree_sitter_mips_external_scanner_deserialize,
     },
     .primary_state_ids = ts_primary_state_ids,
+    .name = "mips",
+    .max_reserved_word_set_size = 0,
+    .metadata = {
+      .major_version = 0,
+      .minor_version = 1,
+      .patch_version = 1,
+    },
   };
   return &language;
 }
